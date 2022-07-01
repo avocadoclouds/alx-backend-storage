19c15c43bb632c87f2f51516f27b3f4c87c7565c
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-01 03:44:42 +0300

    task 3 update

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -7,6 +7,11 @@
 --     Your script can be executed on any database
 
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+-- Lists all bands with Glam rock as their style,
+-- ranked by their longevity
+-- Column names must be: band_name & lifespan
+
+SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
-WHERE style like "%Glam rock%";
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
