b84da5fa1fc4078e444602b80ed3c461325ed569
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-06 05:47:53 +0300

    advanced task

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" Module for using PyMongo """
+"""Module for using PyMongo"""
 
 
 def top_students(mongo_collection):
-    """ Returns all students sorted by average score"""
+    """Returns all students sorted by average score"""
     return list(mongo_collection.find())
