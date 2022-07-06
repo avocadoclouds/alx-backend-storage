c539a89fc18d89030689e9cf03a010e506f5215b
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-06 05:51:22 +0300

    advanced task

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
-"""Module for using PyMongo"""
+"""
+Python function that returns all students sorted by average score:
+  Prototype: def top_students(mongo_collection):
+  mongo_collection will be the pymongo collection object
+  The top must be ordered
+  The average score must be part of each item returns with key = averageScore
+"""
 
 
 def top_students(mongo_collection):
-    """Returns all students sorted by average score"""
-    return list(mongo_collection.find())
+    """
+    Returns all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
